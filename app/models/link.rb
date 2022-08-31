a9d8baa83f4bf7f4ddfb068020526cf7d74f5ae7
Suraj Vadgama <suninthesky@users.noreply.github.com>
2022-08-31 12:30:48 +0100

    Add unit tests for Link

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,9 +1,6 @@
 class Link < ApplicationRecord
   belongs_to :linkable, polymorphic: true
 
-  validates :url, presence: true, format: {
-    with: URI::DEFAULT_PARSER.make_regexp(%w[http https]),
-    message: 'enter a valid website address e.g. http://www.example.com'
-  }
   validates :name, presence: true
+  validates :url, url: { message: 'enter a valid website address e.g. http://www.example.com' }
 end
---
+++ test/models/link_test.rb
@@ -0,0 +1,24 @@
+require "test_helper"
+
+class LinkTest < ActiveSupport::TestCase
+  setup { @subject = links(:link_goal_1) }
+
+  test 'belongs to linkable as goal' do
+    assert_instance_of(Goal, @subject.linkable)
+    assert_present(:linkable, msg: 'must exist')
+  end
+
+  test 'belongs to linkable as project' do
+    @subject = links(:link_project_1)
+    assert_instance_of(Project, @subject.linkable)
+    assert_present(:linkable, msg: 'must exist')
+  end
+
+  test('name required') { assert_present(:name) }
+
+  test 'url' do
+    @subject.url = 'www.bad.url'
+    @subject.valid?
+    assert_error(:url, 'enter a valid website address e.g. http://www.example.com')
+  end
+end
