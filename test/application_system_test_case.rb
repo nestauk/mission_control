c356873461337feaaa03f7af4167dbc0a8839a4a
Laura Murria <laura.murria@nesta.org.uk>
2022-09-09 17:12:00 +0100

    Indicator update test work in progress (drafted but not working - incorrect route/path)

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :headless_chrome, screen_size: [1280, 800]
+  driven_by :selenium, using: :chrome, screen_size: [1280, 800]
 
   OmniAuth.config.test_mode = true
   OmniAuth.config.mock_auth[:okta] = OmniAuth::AuthHash.new({
--- test/system/indicators_test.rb
+++ test/system/indicators_test.rb
@@ -3,6 +3,7 @@ require 'application_system_test_case'
 class IndicatorsTest < ApplicationSystemTestCase
   setup do
     @project = projects(:project_1)
+    @indicator =indicators(:indicator_project_1)
     # sign_in
   end
 
@@ -15,12 +16,26 @@ class IndicatorsTest < ApplicationSystemTestCase
   test 'indicators#create' do
     visit project_path(@project)
     click_link 'Add indicator'
-    fill_in :indicator_title, with: 'Title'
+    fill_in :indicator_title, with: 'Indicator title'
     fill_in :indicator_target_value, with: 1000
     fill_in :indicator_unit, with: '£'
     click_button 'Create'
     assert_text 'Progress indicator created'
     assert_current_path project_path(Project.last)
+    assert_text 'Indicator title'
+  end
+
+  test 'indicators#update' do
+    visit project_path(@project)
+    click_link @indicator.title
+    click_link 'Edit', href: edit_project_indicator_path(@indicator.id)
+    fill_in :indicator_title, with: 'Updated title'
+    fill_in :indicator_description, with: 'This is what the indicator is all about'
+    click_button 'Update'
+    assert_text 'Progress indicator updated'
+    assert_text 'Updated title'
+    assert_text 'This is what the indicator is all about'
+    assert_current_path project_path(@project)
   end
 
 end
