6b5f37c1c0ec60db0f3b3d475b8aca37a4c948ba
Laura Murria <laura.murria@nesta.org.uk>
2022-09-14 15:31:05 +0100

    Indicators create and update working, delete is failing as incorrect path

--- test/system/indicators_test.rb
+++ test/system/indicators_test.rb
@@ -4,14 +4,13 @@ class IndicatorsTest < ApplicationSystemTestCase
   setup do
     @project = projects(:project_1)
     @indicator =indicators(:indicator_project_1)
-    # sign_in
+    sign_in
   end
 
-  # TODO wip test cannot be checked or run until okta setup fix
-  # test 'indicators#new requires sign in' do
-  #   click_button 'Sign out'
-  #   assert_requires_sign_in(new_project_indicator_path(@project))
-  # end
+  test 'indicators#new requires sign in' do
+    click_button 'Sign out'
+    assert_requires_sign_in(new_project_indicator_path(@project))
+  end
 
   test 'indicators#create' do
     visit project_path(@project)
@@ -19,8 +18,7 @@ class IndicatorsTest < ApplicationSystemTestCase
     fill_in :indicator_title, with: 'Indicator title'
     fill_in :indicator_target_value, with: 1000
     fill_in :indicator_unit, with: '£'
-    save_and_open_page
-    fill_in :indicator_description, with: 'First stab at describing'
+    find("trix-editor").set('First stab at describing')
     click_button 'Create'
     assert_text 'Progress indicator created'
     assert_current_path project_path(Project.last)
@@ -32,7 +30,7 @@ class IndicatorsTest < ApplicationSystemTestCase
     click_link @indicator.title
     click_link 'Edit', href: edit_project_indicator_path(project_id: @project.id, id: @indicator.id)
     fill_in :indicator_title, with: 'Updated title'
-    fill_in :indicator_description, with: 'This is what the indicator is all about'
+    find("trix-editor").set('This is what the indicator is all about')
     click_button 'Update'
     assert_text 'Progress indicator updated'
     assert_text 'Updated title'
